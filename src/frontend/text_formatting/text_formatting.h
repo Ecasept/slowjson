eeb7fec4e5474ecc4d2d796228e7a274fe5b83b3
franz-xyz <250022304+franz-xyz@users.noreply.github.com>
2025-12-27 20:39:45 +0100

    complete program changed to widechar-characters

--- src/frontend/text_formatting/text_formatting.h
+++ src/frontend/text_formatting/text_formatting.h
@@ -3,34 +3,35 @@
 
 #include <stdio.h>
 #include "../../midend/data.h"
+#include <wchar.h>
 
 // Textfarben
-#define TXT_RED "\033[31m"
-#define TXT_GREEN "\033[32m"
-#define TXT_YELLOW "\033[33m"
-#define TXT_BLUE "\033[34m"
-#define TXT_MAGENTA "\033[35m"
-#define TXT_CYAN "\033[36m"
-#define TXT_WHITE "\033[37m"
+#define TXT_RED L"\033[31m"
+#define TXT_GREEN L"\033[32m"
+#define TXT_YELLOW L"\033[33m"
+#define TXT_BLUE L"\033[34m"
+#define TXT_MAGENTA L"\033[35m"
+#define TXT_CYAN L"\033[36m"
+#define TXT_WHITE L"\033[37m"
 
 // Hintergrundfarben
-#define BACK_RED "\033[41m"
-#define BACK_GREEN "\033[42m"
-#define BACK_YELLOW "\033[43m"
-#define BACK_BLUE "\033[44m"
-#define BACK_MAGENTA "\033[45m"
-#define BACK_CYAN "\033[46m"
-#define BACK_WHITE "\033[47m"
+#define BACK_RED L"\033[41m"
+#define BACK_GREEN L"\033[42m"
+#define BACK_YELLOW L"\033[43m"
+#define BACK_BLUE L"\033[44m"
+#define BACK_MAGENTA L"\033[45m"
+#define BACK_CYAN L"\033[46m"
+#define BACK_WHITE L"\033[47m"
 
 // Text Styles
-#define TXT_BOLD "\033[1m"
-#define TXT_DIM "\033[2m"
-#define TXT_ITALIC "\033[3m"
-#define TXT_UNDERLINED "\033[4m"
-#define TXT_INVERSE "\033[7m"
+#define TXT_BOLD L"\033[1m"
+#define TXT_DIM L"\033[2m"
+#define TXT_ITALIC L"\033[3m"
+#define TXT_UNDERLINED L"\033[4m"
+#define TXT_INVERSE L"\033[7m"
 
 // Text Farben/Styles zurücksetzen
-#define END_STYLE "\033[0m"
+#define END_STYLE L"\033[0m"
 
 
 // Sortierung Veranstaltungsübersicht
@@ -45,6 +46,7 @@ void print_endscreen();
 int print_averagescreen();
 void clear_display();
 void print_overview_by_time(struct Veranstaltung *ver, size_t size_ver);
+void print_overview_by_mod(struct Veranstaltung *ver, size_t size_ver, struct Modulgruppe *mod, size_t size_mod);
 
 
 
