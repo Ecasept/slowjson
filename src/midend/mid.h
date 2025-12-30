b19ce113ed59cf00759fb92885752dfcabb5c6ba
Queraxe <adrian.jonkov@gmail.com>
2025-12-30 13:20:06 +0100

    feat: sortieralgorithmus

---
+++ src/midend/mid.h
@@ -0,0 +1,12 @@
+#ifndef MID_H
+#define MID_H
+
+#include "data.h"
+#include <stddef.h>
+
+double notendurchschnitt_dumm(struct Veranstaltung *ver, size_t size_ver);
+double notendurchschnitt_po(struct Veranstaltung *ver, size_t size_ver, struct Modulgruppe *mod, size_t size_mod);
+struct Veranstaltung *sort_by_time(struct Veranstaltung *ver, size_t size_ver);
+struct Veranstaltung *sort_by_mod(struct Veranstaltung *ver, size_t size_ver);
+
+#endif
