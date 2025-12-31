f15b4485df07f1109739453e3f250bfc5311aa53
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-12-31 18:19:53 +0100

    feat: implement necessary string functions

--- src/utils/dstring.h
+++ src/utils/dstring.h
@@ -18,6 +18,7 @@ typedef struct string string;
 
 void string_free(string *str);
 void string_new(string *str, const char *source);
+string string_newr(const char *source);
 
 void string_append(string *str, const string *other);
 void string_append_uchar(string *str, uchar other);
@@ -25,8 +26,11 @@ void string_append_cstr(string *str, const char *cstr);
 void string_eq(const string *str, const string *other, bool *res);
 Result string_substr(const string *str, string *out, size_t start,
 					 size_t length);
+void string_clone(const string *str, string *out);
 
 void string_to_cstr(const string *str, char **cstr);
 
 void string_at(const string *str, size_t index, uchar *out);
 Result string_at_err(const string *str, size_t index, uchar *out);
+
+bool string_eq_cstr(const string *str, const char *cstr);
