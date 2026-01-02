ebf6abbbcea9347216d3ab895d79545a1a0b0c9d
Queraxe <adrian.jonkov@gmail.com>
2026-01-02 12:05:50 +0100

    bug: mid.h fehlende funktion hinzugefügt

--- src/midend/mid.h
+++ src/midend/mid.h
@@ -8,5 +8,6 @@ double notendurchschnitt_dumm(struct Veranstaltung *ver, size_t size_ver);
 double notendurchschnitt_po(struct Veranstaltung *ver, size_t size_ver, struct Modulgruppe *mod, size_t size_mod);
 void sort_by_time(struct Veranstaltung *ver, size_t size_ver);
 void sort_by_mod(struct Veranstaltung *ver, size_t size_ver);
+void sort_mod_by_alpha(struct Modulgruppe *mod, size_t size_mod);
 
 #endif
