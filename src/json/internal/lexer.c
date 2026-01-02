ea443717208605dea78bb13e5176317bbeb93830
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-01-02 17:13:46 +0100

    refactor: use correct format specifiers

--- src/json/internal/lexer.c
+++ src/json/internal/lexer.c
@@ -357,7 +357,7 @@ static Result lexer_lex_string(Lexer *lexer, JSONToken *token) {
 		if (chr <= 0x1F) {
 			string_free(&value);
 			return new_errorf(
-				"Encountered control character with value %d in string at line %zu, column %zu",
+				"Encountered control character with value %u in string at line %zu, column %zu",
 				ELexerSyntaxError, chr, lexer->line, lexer->column - 1);
 		}
 
@@ -577,7 +577,7 @@ static bool num_dfa_next_state(enum NumParseState current_state, UCP chr,
 		// End reached
 		return false;
 	default:
-		panicf("Invalid NumParseState: %d", current_state);
+		panicf("Invalid NumParseState: %u", current_state);
 	}
 	return true;
 }
@@ -670,7 +670,7 @@ void lexer_free_token(JSONToken *token) { string_free(&token->value); }
 
 char *tk_as_str(JSONTokenType type) {
 	if (type < 0 || type >= sizeof(JSONTokenTypeStrings) / sizeof(char *)) {
-		panicf("Invalid JSONTokenType: %d", type);
+		panicf("Invalid JSONTokenType: %u", type);
 	}
 	return (char *)JSONTokenTypeStrings[type];
 }
