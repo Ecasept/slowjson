5e0ac2bdd46fad938d9979a385b7303388d8c43b
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-01-13 21:06:01 +0100

    fix: invalid unicode at the end causing the program to think it hit the end of the json file and terminating correctly (instead of reporting invalid unicode)

--- src/json/internal/lexer/utils.c
+++ src/json/internal/lexer/utils.c
@@ -2,13 +2,13 @@
 #include "../../utils/unicode/utf8.h"
 
 Result lexer_peek(const Lexer *lexer, UCP *out) {
+	if (lexer->position >= lexer->source->arr.length) {
+		return new_error("Unexpected end of input", ELexerEOF);
+	}
 	size_t pos_copy =
 		lexer->position; // Don't pass the actual position. We are just peeking
 	Result r = utf8_get_next_codepoint(as_sv(*lexer->source), &pos_copy, out);
-	if (r.type == EUnicodeUnexpectedEndOfString) {
-		error_free(r);
-		return new_error("Unexpected end of input", ELexerEOF);
-	} else if (!r.success) {
+	if (!r.success) {
 		return r;
 	}
 	return new_success();
